3cb2c328fb638cb966ad3900a92844db499c986e
Emrah Çetin <82283064+emrahcetin07@users.noreply.github.com>
2022-11-18 03:25:09 +0300

    Hws

---
+++ mayınTarlası/src/mayınTarlası/Main.java
@@ -0,0 +1,11 @@
+package mayınTarlası;
+
+public class Main {
+	
+	    public static void main(String args[]) {
+	        MineSweeper mineSweeper = new MineSweeper();
+	        mineSweeper.Run();
+	    }
+	
+
+}
